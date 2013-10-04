58564a331562f475e2f557abfb5624b49a7ccc6a
capgeminigit <krish3693@gmail.com>
2013-10-04 12:38:33 +0530

    Update dir-dnd.js

--- app/dir-dnd.js
+++ app/dir-dnd.js
@@ -1,24 +1,58 @@
-
 // directive for dnd between lists
 app.directive('dndBetweenList', function($parse) {
 
-    return function(scope, element, attrs) {
-	$('#sourceList').sortable({
-		connectWith: '#targetList'
-	});
-	$('#targetList').sortable({
-		connectWith: '#sourceList,#targetList1'
-	});
-	
-	$('#targetList1').sortable({
-		connectWith: '#targetList,#targetList2'
-	});
+	return function(scope, element, attrs) {
+		$('#sourceList').sortable({
+			connectWith : '#targetList'
+		});
+		$('#targetList').sortable({
+			connectWith : '#sourceList,#targetList1'
+		});
+
+		$('#targetList1').sortable({
+			connectWith : '#targetList,#targetList2'
+		});
 
 		$('#targetList2').sortable({
-		connectWith: '#targetList1'
-	});
+			connectWith : '#targetList1'
+		});
 
+		element.droppable({
+			drop : function(event, ui) {
+				// var uiItem = ui.draggable;
+				// var dragIndex = uiItem.attr("item-index");
+			//	var state = ui.draggable.attr("item-state");
+				// var dragEl = angular.element(ui.draggable).parent();
 
+				var dragId = ui.draggable.attr("item-id");
+				var dragstate = ui.draggable.attr("item-state");
+				var dropEl = angular.element(this);
+				var dropState = dropEl.attr("state");
 
-    }
+				var scope = ui.draggable.scope();
+				var store = scope.taskStore;
+				var index = -1;
+				for ( var i = 0; i < store.length; i++) {
+					if (dragId == store[i].id) {
+						index = i;
+						break;
+					}
+				}
+				if (index != -1) {
+				
+				var start =Number(dragstate)+1;
+				var end =Number(dragstate-1);
+				alert(dropState +"----"+start+"----"+end);
+				if(dropState == start || dropState == end){
+					store[i].state = dropState;
+					scope.$apply();
+					}
+					else{
+					store[i].state = Number(dragstate)+1;
+					}
+				}
+				console.log("dropped");
+			}
+		});
+	};
 });
