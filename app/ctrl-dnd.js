86da3c679c7121301fd5ce231f2dba561153c39e
capgeminigit <krish3693@gmail.com>
2013-10-07 09:25:44 +0530

    Update ctrl-dnd.js

--- app/ctrl-dnd.js
+++ app/ctrl-dnd.js
@@ -1,232 +1,167 @@
-function dndCtrl($scope) {
+	var maxId = 11;
+	function dndCtrl($scope) {
 
-	$scope.increment = function(item) {
-		item.count += 1;
-		console.info(item.count);
-	};
-	$scope.userStore = [ {
-		id : 1,
-		name : 'Mats',
-		img : 'resources/images/mats.jpeg'
-	}, {
-		id : 2,
-		name : 'Homer',
-		img : 'resources/images/homer.jpg'
-	}, {
-		id : 3,
-		name : 'Brian',
-		img : 'resources/images/brian.jpeg'
-	} ];
-
-	$scope.taskStore = [ {
-		id : 1,
-		name : 'Fix IE7 bug',
-		state : 0,
-		nbrComments : 0,
-		userId : 1,
-		userName : 'Mats',
-		userImg : 'resources/images/mats.jpeg'
-	}, {
-		id : 2,
-		name : 'Sneak-install Chrome Frame',
-		state : 0,
-		nbrComments : 1,
-		userId : 1,
-		userName : 'Mats',
-		userImg : 'resources/images/mats.jpeg'
-	}, {
-		id : 81,
-		name : 'Sneak-install Chrome Frame1',
-		state : 0,
-		nbrComments : 1,
-		userName : '',
-		userImg : ''
-	}, {
-		id : 12,
-		name : 'Add Windows Phone support',
-		state : 1,
-		nbrComments : 1,
-		userId : 3,
-		userName : 'Brian',
-		userImg : 'resources/images/brian.jpeg'
-	}, {
-		id : 122,
-		name : 'Make App',
-		state : 1,
-		nbrComments : 1,
-		userName : '',
-		userImg : ''
-	}, {
-		id : 3,
-		name : 'Task 222 foo bar lots of text in this one eh? Fooooo',
-		state : 2,
-		nbrComments : 0,
-		userName : '',
-		userImg : 'resources/images/homer.jpg'
-	}, {
-		id : 4,
-		name : 'Find Unicorn',
-		state : 2,
-		nbrComments : 0,
-		userName : 'Homer',
-		userId : 2,
-		userImg : 'resources/images/homer.jpg'
-	}, {
-		id : 5,
-		name : 'IE6 support',
-		state : 1,
-		nbrComments : 0,
-		userName : '',
-		userImg : ''
-	}, {
-		id : 6,
-		name : 'Chrome development',
-		state : 3,
-		nbrComments : 0,
-		userName : '',
-		userImg : 'resources/images/homer.jpg'
-	}, {
-		id : 7,
-		name : 'Find holy grail',
-		state : 3,
-		nbrComments : 1,
-		userName : '',
-		userImg : ''
-	}, {
-		id : 8,
-		name : 'Dig hole',
-		state : 3,
-		nbrComments : 0,
-		userName : '',
-		userImg : ''
-	}, {
-		id : 9,
-		name : 'Eat raisins',
-		state : 3,
-		nbrComments : 3,
-		userName : '',
-		userImg : 'resources/images/homer.jpg'
-	} ];
-
-	$scope.$watch("taskStore", function(name) {
-		console.log("taskStore: " + name.map(function(e) {
-			return e.id;
-
-		}).join(','));
-	}, true);
-
-	$scope.$watch('nstart', function(newValue) {
-		if (angular.isArray(newValue)) {
-			newValue.map(function(e) {
-				$(".control-label" + e.id).dblclick(function() {
-					var placeholder = $(this).html();
-					$(this).hide();
-					$(".input-medium" + e.id).show();
-					$(".input-medium" + e.id).attr('placeholder', placeholder);
-				});
-
-				$(".input-medium" + e.id).blur(function() {
-					var placeholder = $(this).val();
-					$(this).hide();
-					$(".control-label" + e.id).show();
-					$(".control-label" + e.id).html(placeholder);
-					e.name =placeholder;
-				});
+        $scope.taskStore        = [
+            { id : 1,        name : 'Fix IE7 bug', state : 0, nbrComments : 0, userId : 1, userName : 'Mats', userImg : 'resources/images/mats.jpeg'},
+            { id : 2,        name : 'Sneak-install Chrome Frame', state : 0, nbrComments : 1, userId : 1, userName : 'Mats', userImg: 'resources/images/mats.jpeg'},
+             { id : 81,        name : 'Sneak-install Chrome Frame1', state : 0, nbrComments : 1, userName : '', userImg: ''},
+            { id : 12,       name : 'Add Windows Phone support', state : 1, nbrComments : 1, userId : 3, userName : 'Brian', userImg: 'resources/images/brian.jpeg'},
+            { id : 122,      name : 'Make App', state : 1, nbrComments : 1, userName : '',  userImg :''},
+            { id : 3,        name : 'Task 222 foo bar lots of text in this one eh? Fooooo', state : 2, nbrComments : 0, userName : '', userImg :'resources/images/homer.jpg'},
+            { id : 4,        name : 'Find Unicorn', state : 2, nbrComments : 0, userName : 'Homer', userId : 2, userImg : 'resources/images/homer.jpg'},
+            { id : 5,        name : 'IE6 support', state : 1, nbrComments : 0, userName : '', userImg :''},
+            { id : 6,        name : 'Chrome development', state : 3, nbrComments : 0, userName : '', userImg :'resources/images/homer.jpg'},
+            { id : 7,        name : 'Find holy grail', state : 3, nbrComments : 1, userName : '', userImg :''},
+            { id : 8,        name : 'Dig hole', state : 3, nbrComments : 0, userName : '', userImg :''},
+            { id : 9,        name : 'Eat raisins', state : 3, nbrComments : 3, userName : '', userImg :'resources/images/homer.jpg'}
+        ];
+		
+		
+	
+       
+			var taskStore=   $scope.taskStore  ;
+			$scope.dropListener = function (eDraggable, eDroppable) {
+			
+				var eSrc = eDraggable.parent();
+				var sSrc = eSrc.data('stid');
+				var sTarget = eDroppable.data('stid');
+				var modelSrc ="taskStore | filter:{ state: "+sSrc+"}";
+				var   modelTarget ="taskStore | filter:{ state: " +sTarget+" }";
+			 
+               $scope.$apply(function () {
+			    var index = eDraggable.data('index');
+			            var aSrc = $scope.$eval(modelSrc);
+                        var aTarget = $scope.$eval(modelTarget);
+						var item = aSrc[index];
+					
+						 var error = isDropForbidden(sSrc,sTarget);
+                         if (sTarget == (Number(sSrc)+1) || sTarget == (Number(sSrc)-1)){
+						  	item.state =sTarget;
+						 }
+						else {         }
+			   })
+ 
+            }
+			
+			 var isDropForbidden = function (sSrc,sTarget) {
+			    if (sTarget == (Number(sSrc)+1) || sTarget == (Number(sSrc)-1)) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                };
+				
+				
+				
+function afterApply(){
+				
+	$('.sch-task').hover(
+		function () {
+			$(this).css({"background-color":"#EEFFFF","color":"#888888"});
+		//	console.info($(this).find(".Imgwrapper").css("display"));
+			if($(this).find(".Imgwrapper").css("display") == 'none')
+			$(this).find(".Imgwrapper").css("display","block");
+		}, 
+		function () {
+			$(this).css({"background-color":"#FFFFFF","color":"#888888"});
+			//	console.info($(this).find(".sch-user-avatar").attr("src").length);
+				if($(this).find(".sch-user-avatar").attr("src").length == 0)
+				$(this).find(".Imgwrapper").css("display","none");
 			});
-
-			$scope.nstartCount = newValue.length;
-		}
-	}, true);
-
-	$scope.$watch('inProgress', function(newValue) {
-		if (angular.isArray(newValue)) {
-			newValue.map(function(e) {
-				$(".control-label" + e.id).dblclick(function() {
-					var placeholder = $(this).html();
-					$(this).hide();
-					$(".input-medium" + e.id).show();
-					$(".input-medium" + e.id).attr('placeholder', placeholder);
-				});
-
-				$(".input-medium" + e.id).blur(function() {
-					var placeholder = $(this).val();
-					$(this).hide();
-					$(".control-label" + e.id).show();
-					$(".control-label" + e.id).html(placeholder);
-					e.name =placeholder;
-				});
+	 
+		$('.sch-task').click(
+			function () {
+				$(this).css({"background-color":"#7b68ee","color":"#FFFFFF"});
 			});
+			registerUserMenuForAll();
 
-			$scope.inProgressCount = newValue.length;
-		}
-	}, true);
-
-	$scope.$watch('test', function(newValue) {
-		if (angular.isArray(newValue)) {
-			newValue.map(function(e) {
-				$(".control-label" + e.id).dblclick(function() {
-					var placeholder = $(this).html();
-					$(this).hide();
-					$(".input-medium" + e.id).show();
-					$(".input-medium" + e.id).attr('placeholder', placeholder);
-				});
-
-				$(".input-medium" + e.id).blur(function() {
-					var placeholder = $(this).val();
-					$(this).hide();
-					$(".control-label" + e.id).show();
-					$(".control-label" + e.id).html(placeholder);
-					e.name =placeholder;
-				});
-			});
-
-			$scope.testCount = newValue.length;
-		}
-	}, true);
-
-	$scope.$watch('done', function(newValue) {
-		if (angular.isArray(newValue)) {
-			newValue.map(function(e) {
-				$(".control-label" + e.id).dblclick(function() {
-					var placeholder = $(this).html();
-					$(this).hide();
-					$(".input-medium" + e.id).show();
-					$(".input-medium" + e.id).attr('placeholder', placeholder);
-				});
-
-				$(".input-medium" + e.id).blur(function() {
-					var placeholder = $(this).val();
-					$(this).hide();
-					$(".control-label" + e.id).show();
-					$(".control-label" + e.id).html(placeholder);
-					e.name =placeholder;
-				});
-			});
-
-			$scope.doneCount = newValue.length;
-		}
-	}, true);
-
-	$scope.taskStoreEmpty = function() {
-		return $scope.taskStore.length == 0;
-	};
-
-	$scope.addTaskStoreEntry = function(state) {
-		$scope.addEntry($scope.taskStore, "changeUser", state);
-	};
-
-	$scope.addEntry = function(items, imgIdPrefix, state) {
-		var id = Date.now();
-		items.push({
-			"id" : id,
-			"name" : "New Task...",
-			"state" : state,
-			"nbrComments" : 0,
-			"userId" : "",
-			"userName" : "",
-			"userImg" : ""
+		$('.sch-task').editables({ 
+			beforeEdit: function(field){
+			if(this.data('updatable')) field.val(this.text());
+			},
+			beforeFreeze: function(display){ 
+			if(display.data('updatable')) display.text(this.val());
+			}
 		});
-		registerUserMenu(imgIdPrefix + id);
-	};
-
-}
+			
+		$('.sch-user-avatar').each(function(i, obj) {
+			var fileext=obj.src;
+			var n=fileext.lastIndexOf(".");
+			var fileType="";
+			if(n != -1)
+				fileType=fileext.substring(n+1);
+				if(fileType.length !=0 && fileType == 'html'){
+					$(obj.parentNode).css("display","none");
+				}
+			else
+				$(obj.parentNode).css("display","block");
+			});
+				}
+					$scope.$watch("taskStore", function(name) {
+				}, true);
+	
+		
+			$scope.$watch('nstart', function (newValue) {
+				if (angular.isArray(newValue)) {
+					newValue.map(function(e){
+					afterApply();
+					e.name=$("#input-medium"+e.id).html();
+					});
+					$scope.nstartCount=newValue.length;
+				}
+			}, true);
+	
+			$scope.$watch('inProgress', function (newValue) {
+				if (angular.isArray(newValue)) {
+					newValue.map(function(e){
+						afterApply();
+						e.name=$("#input-medium"+e.id).html();
+					});
+					$scope.inProgressCount=newValue.length;
+				}
+			}, true);
+	
+			$scope.$watch('test', function (newValue) {
+				if (angular.isArray(newValue)) {
+					newValue.map(function(e){
+						afterApply();
+						e.name=$("#input-medium"+e.id).html();
+					});
+					$scope.testCount=newValue.length;
+				}
+			}, true);
+	
+			$scope.$watch('done', function (newValue) {
+				if (angular.isArray(newValue)) {
+					newValue.map(function(e){
+						afterApply();
+						e.name=$("#input-medium"+e.id).html();
+				});
+				
+				$scope.doneCount=newValue.length;
+				}
+			}, true);
+	
+	
+			$scope.taskStoreEmpty = function() {
+				return $scope.taskStore.length == 0;
+			};
+	
+			$scope.addTaskStoreEntry = function(state) {
+				$scope.addEntry($scope.taskStore, "changeUser",state);
+			};
+	
+			$scope.addEntry = function(items, imgIdPrefix,state) {
+				var id = Date.now();
+				items.push({
+					"id" : id,
+					"name" : "New Task...",
+					"state" :state,
+					"nbrComments" : 0,
+					"userId" : "", 
+					"userName" : "", 
+					"userImg" : ""         
+				});
+				registerUserMenu(imgIdPrefix + id);
+			};
+	}
