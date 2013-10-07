74d473d0c3f6076971eededf8efe704fb29c096a
capgeminigit <krish3693@gmail.com>
2013-10-07 09:26:39 +0530

    Update dir-dnd.js

--- app/dir-dnd.js
+++ app/dir-dnd.js
@@ -1,78 +1,127 @@
-// directive for dnd between lists
-app.directive('dndBetweenList', function($parse) {
 
-	return function(scope, element, attrs) {
-		$('#sourceList').sortable({
-			connectWith : '#targetList'
+var userStore = {
+	width : 150,
+	items : [ {
+		text : "Mats",
+		icon : "img/mats.jpeg",
+		action : menuAction
+	}, {
+		text : "Homer",
+		icon : "img/homer.jpg",
+		action : menuAction
+	}, {
+		text : "Brian",
+		icon : "img/brian.jpeg",
+		action : menuAction
+	} ]
+};
+
+	function menuAction(target) {
+		target.src = this.data.icon;
+		var scope =angular.element(target).scope();
+	//	console.info(angular.element(target).scope());
+	//	console.info(scope.this.item);
+		scope.this.item.userImg =this.data.icon;
+		$(target.parentNode).css("display","block");
+
+	}
+
+
+// Registering the menu for all images.
+function registerUserMenuForAll() {
+	var items = $(".context-menu-one");
+	for ( var i = 0; i < items.length; i++) {
+		$(items[i]).contextmenu(userStore);
+	}
+}
+
+function registerUserMenu() {
+	setTimeout('registerUserMenuForAll()', 500);
+}
+
+
+
+			
+
+	$().ready(function() {
+	
+		registerUserMenuForAll();
+
+		$("#kanban-dnd-div .columnCollapsed").hide();
+		
+		$('.sch-user-avatar').each(function(i, obj) {
+			var fileext=obj.src;
+			var n=fileext.lastIndexOf(".");
+			var fileType="";
+			if(n != -1)
+				fileType=fileext.substring(n+1);
+				if(fileType.length !=0 && fileType == 'html'){
+					$(obj.parentNode).css("display","none");
+				}
+			else
+				$(obj.parentNode).css("display","block");
+			});
 		});
-		$('#targetList').sortable({
-			connectWith : '#sourceList,#targetList1'
+	
+		var totWidth = 92;
+		var expandedWidth = 23;
+		var collapsedWidth = 4;
+
+		$("#kanban-dnd-div .columnCollapsed").click(function(ui, event) {
+			var uiItem = $($(this).next());
+			uiItem.show();
+			uiItem.animate({
+				width : "100%"
+			}, {
+				duration : 300,
+				queue : false
+			});
+			
+			$(uiItem.parent()).attr("expanded", "true");
+			var collapsedItem = $("#" + uiItem.attr("id") + "Collapsed");
+			collapsedItem.hide();
+			
+			// Decreasing collapsed divs widths.
+			var collapsedDivs = $("div[expanded=false]");
+			for(var i = 0; i < collapsedDivs.length; i++) {
+				$(collapsedDivs[i]).width(collapsedWidth + "%");
+			}
+			
+			// Increasing expanded divs widths.
+			var expandedDivs = $("div[expanded=true]");
+			var expandedCount = expandedDivs.length;
+			var widthToSet = (totWidth - ((4 - expandedCount) * collapsedWidth)) / expandedCount;
+			for(var i = 0; i < expandedDivs.length; i++) {
+				$(expandedDivs[i]).width(widthToSet + "%");
+			}
 		});
 
-		$('#targetList1').sortable({
-			connectWith : '#targetList,#targetList2'
+		$("img[type=collapseColumn]").click(function(ui, event) {
+			var uiItem = $($(this).parent().parent().parent());
+			uiItem.hide();
+			uiItem.animate({
+				width : "0%"
+			}, {
+				duration : 300,
+				queue : false
+			});
+			
+			$(uiItem.parent()).attr("expanded", "false");
+			var collapsedItem = $("#" + uiItem.attr("id") + "Collapsed");
+			collapsedItem.show();
+			
+			// Decreasing collapsed divs widths.
+			var collapsedDivs = $("div[expanded=false]");
+			for(var i = 0; i < collapsedDivs.length; i++) {
+				$(collapsedDivs[i]).width(collapsedWidth + "%");
+			}
+			
+			// Increasing expanded divs widths.
+			var expandedDivs = $("div[expanded=true]");
+			var expandedCount = expandedDivs.length;
+			var widthToSet = (totWidth - ((4 - expandedCount) * collapsedWidth)) / expandedCount;
+			for(var i = 0; i < expandedDivs.length; i++) {
+				$(expandedDivs[i]).width(widthToSet + "%");
+			}
 		});
-
-		$('#targetList2').sortable({
-			connectWith : '#targetList1'
-		});
-
-		var possibleToStates = new Array();
-		possibleToStates[0] = [ 1 ];
-		possibleToStates[1] = [ 0, 2 ];
-		possibleToStates[2] = [ 1, 3 ];
-		possibleToStates[3] = [ 2 ];
-
-		element
-				.droppable({
-					drop : function(event, ui) {
-						// var uiItem = ui.draggable;
-						// var dragIndex = uiItem.attr("item-index");
-						// var dragEl = angular.element(ui.draggable).parent();
-
-						var currentState = parseInt(ui.draggable
-								.attr("item-state"));
-						var dragId = ui.draggable.attr("item-id");
-						var dropEl = angular.element(this);
-						var dropState = parseInt(dropEl.attr("state"));
-
-						if (currentState == dropState) {
-						return;
-						}
-						var scope = ui.draggable.scope();
-						var store = scope.taskStore;
-						var index = -1;
-						for ( var i = 0; i < store.length; i++) {
-							if (dragId == store[i].id) {
-								index = i;
-								break;
-							}
-						}
-						if (index != -1) {
-							if (possibleToStates[currentState]
-									.indexOf(dropState) != -1) {
-								store[index].state = dropState;
-							} else {
-								// event.stopPropagation();
-								// event.preventDefault();
-								var states = possibleToStates[currentState];
-								for ( var i = dropState; i >= 0; i--) {
-									if (states.indexOf(i) != -1) {
-										store[index].state = i;
-										break;
-									}
-								}
-								for ( var i = dropState; i <= 3; i++) {
-									if (states.indexOf(i) != -1) {
-										store[index].state = i;
-										break;
-									}
-								}
-							}
-						}
-						scope.$apply();
-						console.log("dropped");
-					}
-				});
-	};
-});
+		
